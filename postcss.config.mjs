ee346e94e37e29b05aad2c534f93f60f75eb83ce
sakuoka <s.atsushi999@gmail.com>
2025-12-04 13:10:22 +0900

    fix: install missing autoprefixer

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {}, // ← ここを変更しました
-    autoprefixer: {},
+    '@tailwindcss/postcss': {}, // ← これはOK
+    autoprefixer: {},           // ← これを動かすためにインストールしました
   },
 };
 
